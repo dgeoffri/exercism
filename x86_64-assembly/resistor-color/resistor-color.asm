b7c16ab00ac9fe2e9699858131729a409301c313
DG <dgeoffri@david.geoffrions.com>
2021-11-14 22:38:56 -0600

    now it works

---
+++ x86_64-assembly/resistor-color/resistor-color.asm
@@ -0,0 +1,64 @@
+section .rodata
+
+black		db	"black",0
+brown		db	"brown",0
+red		db	"red",0
+orange		db	"orange",0
+yellow		db	"yellow",0
+green		db	"green",0
+blue		db	"blue",0
+violet		db	"violet",0
+grey		db	"grey",0
+white		db	"white",0
+
+color_array:
+		dq	black, brown, red, orange, yellow, green, blue, violet, grey, white, 0
+
+section .text
+global colors
+colors:
+		lea	rax, [rel color_array]
+		ret
+
+global color_code
+color_code:
+		push	rbx
+ 		xor	rbx, rbx
+.l1:
+		lea	rcx, [rel color_array]
+		mov	rsi, [rcx + rbx * 8]
+		mov	rax, [rsi]
+		test	rax, rax
+		jz	.failed
+		push	rdi
+		call	compare
+		pop	rdi
+		test	rax, rax
+		jz	.done
+		inc	rbx
+		jmp	.l1
+.failed:
+		mov	rbx, -1
+.done:
+		mov	rax, rbx
+		pop	rbx
+		ret
+
+compare:
+.l1:
+		mov	al, BYTE [rsi]
+		mov	ah, BYTE [rdi]
+		cmp	al, ah	
+		jnz	.failed
+		test	ah, ah
+		jz	.matched
+		inc	rsi
+		inc	rdi
+		jmp	.l1
+.failed:
+		mov	rax, 1
+		jmp	.done
+.matched:
+		xor	rax, rax
+.done:
+		ret
---
+++ x86_64-assembly/resistor-color/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern const char **colors(void);
+extern int color_code(const char *color);
+
+int
+main (int argc, char **argv) {
+	const char **colorarray = colors();
+	const char *color;
+	do {
+		// printf ("%s\n", *colorarray);
+		printf ("A %s stripe has value %d\n", *colorarray, color_code(*colorarray));
+	} while *colorarray++;
+}
